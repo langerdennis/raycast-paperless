4dd6cfe88b7fff3a8e61d99929a5a875ba5f8479
Dennis Langer <dennis.langer@sap.com>
2022-04-15 21:41:05 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { getPreferenceValues, ActionPanel, Detail, List, Action } from "@raycast/api";
+
+export interface Preferences {
+  paperlessURL: string;
+  apiToken: string;
+}
+const { paperlessURL }: Preferences = getPreferenceValues();
+const { apiToken }: Preferences = getPreferenceValues();
 
 export default function Command() {
   return (
     <List>
       <List.Item
         icon="list-icon.png"
-        title="Greeting"
+        title="Hi there"
         actions={
           <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
+            <Action.Push title="Show Details" target={<Detail markdown="# Hey!👋" />} />
           </ActionPanel>
         }
       />
