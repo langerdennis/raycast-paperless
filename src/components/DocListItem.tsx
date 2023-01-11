713a7e0eaf5f7b9e452597862ae002542a2c8a4b
AZE <adam.zerdoumi@gmail.com>
2023-01-11 22:01:11 +0100

    DocListItem displays more document info and uses dateFormat

--- src/components/DocListItem.tsx
+++ src/components/DocListItem.tsx
@@ -8,18 +8,37 @@ import {
 import {paperlessDocumentResults} from '../models/paperlessResponse.model';
 import {Preferences} from '../models/preferences.model';
 import {DocActions} from './DocActions';
+import {DocItem} from '../models/docItem.model';
+import moment from 'moment';
 
 const {paperlessURL}: Preferences = getPreferenceValues();
+const {dateFormat}: Preferences = getPreferenceValues();
 
-interface DocListItemProps {
-    document: paperlessDocumentResults;
+const formatDateTime = (date: string): string => {
+    console.log(date);
+    return moment(date).format(dateFormat).toString();
 }
 
-export const DocListItem = ({document}: DocListItemProps): JSX.Element => {
+export const DocListItem = ({document, type, tags, correspondent}: DocItem): JSX.Element => {
     return (
         <List.Item
             title={document.title}
             icon={Icon.Document}
+            detail={
+                <List.Item.Detail
+                    metadata={
+                        <List.Item.Detail.Metadata>
+                            <List.Item.Detail.Metadata.Label title="Type" text={type}/>
+                            <List.Item.Detail.Metadata.Label title="Correspondent" text={correspondent}/>
+                            <List.Item.Detail.Metadata.Label title="Tags" text={tags}/>
+                            <List.Item.Detail.Metadata.Separator/>
+                            <List.Item.Detail.Metadata.Label title="Added" text={formatDateTime(document.added)}/>
+                            <List.Item.Detail.Metadata.Label title="Created" text={formatDateTime(document.created)}/>
+                            <List.Item.Detail.Metadata.Label title="Modified" text={formatDateTime(document.modified)}/>
+                        </List.Item.Detail.Metadata>
+                    }
+                />
+            }
             actions={
                 <DocActions document={document}/>
             }
